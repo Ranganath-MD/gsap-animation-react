7addf2a2183eb87f4ba61608c1f3cd03f56fd6e3
Ranga <ranga@mverve.com>
2020-06-22 12:19:21 +0530

    Updates

---
+++ precache-manifest.5bd922ccc30a35980b11bfc183c03470.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4f8a10e63d0e6dbae2796b59decc3c",
+    "url": "/gsap-animation-react/index.html"
+  },
+  {
+    "revision": "8657ffbcf8275545e92d",
+    "url": "/gsap-animation-react/static/css/main.b438a3f5.chunk.css"
+  },
+  {
+    "revision": "b59d42a307577ea92c9c",
+    "url": "/gsap-animation-react/static/js/2.bbac602c.chunk.js"
+  },
+  {
+    "revision": "79780c1c5b94754b0976317971f3fe6b",
+    "url": "/gsap-animation-react/static/js/2.bbac602c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8657ffbcf8275545e92d",
+    "url": "/gsap-animation-react/static/js/main.06b7d550.chunk.js"
+  },
+  {
+    "revision": "b12d90f3d0526026645d",
+    "url": "/gsap-animation-react/static/js/runtime-main.7dde6571.js"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/gsap-animation-react/static/media/logo.9a28da9f.svg"
+  },
+  {
+    "revision": "4322bd96bd493862281782a076747b20",
+    "url": "/gsap-animation-react/static/media/newyork.4322bd96.png"
+  }
+]);
